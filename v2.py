9dbf04ab48711f25759a912f07887685f1a76a34
tombackert <backert.github@gmail.com>
2023-03-24 14:25:22 +0100

    multi headed self-attention, reach 2.28 val loss

--- v2.py
+++ v2.py
@@ -87,6 +87,17 @@ class Head(nn.Module):
         out = wei @ v # (B, T, T) @ (B, T, C) --> (B, T, C) 
         return out
 
+class MultiHeadAttention(nn.Module):
+    """multiple heads of self-attention in parallel"""
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+
+    def forward(self, x):
+        return torch.cat([h(x) for h in self.heads], dim=-1)
+
+
 # feeding the input into a neural net
 # biagram model 
 class BigramLanguageModel(nn.Module):
@@ -96,7 +107,7 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_head = Head(n_embd)
+        self.sa_heads = MultiHeadAttention(4, n_embd//4) # 4 heads of 8-dimensional self-attention
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -106,7 +117,7 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) # (B,T,C)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
         x = tok_emb + pos_emb # (B,T,C)
-        x = self.sa_head(x) # apply one head of self-attention (B,T,C)
+        x = self.sa_heads(x) # apply one head of self-attention (B,T,C)
         logits = self.lm_head(x) # (B,T,vocab_size)
 
         if targets is None:
